933a0d5b3e21cb70ce0d8231c93d0acad102f7c3
DanielCArce <daniel.camposarce@gmail.com>
2024-06-23 16:41:50 -0600

    add types to components

--- app/(blog)/blog/[slug]/page.tsx
+++ app/(blog)/blog/[slug]/page.tsx
@@ -1,15 +1,11 @@
 import { getAllPosts, getPostContent } from "@/libs/readPosts"
 import SocialWidget from '@/components/SocialWidget'
 import {MDXRemote} from 'next-mdx-remote/rsc'
-type Components = {
-    children: JSX.Element
+const H1Component = (props: any) => {
+    return (<h1 {...props}>{ props.children}</h1>)
 }
-const components = {
-  h1: (props :Components) => (
-    <h1 {...props} className="text-2xl font-bold uppercase">
-      {props.children}
-    </h1>
-  ),
+const CustomComponents = {
+    H1Component
 }
 export async function generateStaticParams() {
     const allPosts = await getAllPosts()
@@ -27,7 +23,7 @@ async function Page({ params }: { params: { slug: string } }){
     const {content} = await getPostContent(params.slug)
     return (
         <>
-            <MDXRemote source={content} options={{parseFrontmatter:true}} components={components}/>
+            <MDXRemote source={content} options={{parseFrontmatter:true}} components={CustomComponents}/>
         <SocialWidget/>
         </>
     )
