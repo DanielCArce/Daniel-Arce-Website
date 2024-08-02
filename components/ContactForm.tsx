ad693f9501f59d193462ab054915aeea573ea13d
DanielCArce <daniel.camposarce@gmail.com>
2024-08-02 12:57:10 -0600

    remove formik to form

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,42 +1,29 @@
 "use client";
 import React from "react";
-import { Formik } from "formik";
+import {sendEmail} from '@/actions/formActions'
 function ContactForm() {
   return (
-    <Formik
-      initialValues={{ name: "", email: "", message: "", phone: "" }}
-      onSubmit={(values, helpers) => {
-        console.log({ values });
-      }}
-    >
-      {({ handleBlur, handleChange, handleSubmit }) => {
-        return (
-          <div className="text-black">
-            <div className="flex flex-col gap-1">
-              <label htmlFor="name">Name</label>
-              <input
-                id="name"
-                name="name"
-                placeholder="Write your name"
-                onBlur={handleBlur}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="flex flex-col gap-1">
-              <label htmlFor="email">Email</label>
-              <input
-                id="email"
-                name="email"
-                placeholder="Write your email"
-                onBlur={handleBlur}
-                onChange={handleChange}
-              />
-            </div>
-          </div>
-        );
-      }}
-    </Formik>
-  );
+    <>
+      <h3 className="text-2xl font-bold uppercase font-sans text-black mb-3 text-center">Contact</h3>
+    <form action={sendEmail} className="text-black text-xl flex flex-col items-center justify-center gap-5">
+      <div className="flex flex-col gap-1">
+        <label htmlFor="name">Name</label>
+        <input type="text" className="w-72 border-2" name="name" id="name"/>
+      </div>
+      <div className="flex flex-col gap-1">
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" id="email" className="w-72 border-2"/>
+      </div>
+      <div className="flex flex-col gap-1">
+        <label htmlFor="message">Message</label>
+        <textarea name="message" id="message" className="w-72 border-2"></textarea>
+      </div>
+      <div className="flex flex-col gap-1 mb-10">
+        <button className="py-2 px-4 bg-cyan-700 text-slate-800">Send Message</button>
+      </div>
+  </form>
+    </>
+)
 }
 
 export default ContactForm;
