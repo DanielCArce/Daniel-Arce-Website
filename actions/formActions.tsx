968d747227e0c91df0acc3a4c0cd99cb8f646664
DanielCArce <daniel.camposarce@gmail.com>
2024-08-02 12:57:40 -0600

    add action for contact form

--- actions/formActions.tsx
+++ actions/formActions.tsx
@@ -1,4 +1,13 @@
-export function sendEmail(formData:FormData){
-    'use server'
+'use server'
+import { Resend } from 'resend';
+export async function sendEmail(formData:FormData){
+    console.log({ formData })
 
+const resend = new Resend(process.env.RESEND_KEY);
+    resend.emails.send({
+        from: "daniel.camposarce@gmail.com",
+        to: "daniel.camposarce@gmail.com",
+        subject: " New Message From Website",
+        text:formData.message
+    })
 }
