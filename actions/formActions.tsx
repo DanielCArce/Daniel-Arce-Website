23aa7af16c925926cbac81469c6897c7d1d1494e
DanielCArce <daniel.camposarce@gmail.com>
2024-08-03 14:01:32 -0600

    temp fix

--- actions/formActions.tsx
+++ actions/formActions.tsx
@@ -1,13 +1,11 @@
 'use server'
-import { Resend } from 'resend';
-export async function sendEmail(formData:FormData){
+import { RESEND_KEY } from '@/config/constants'
+import {Resend} from 'resend'
+export async function sendEmail(formData: FormData) {
+    const mailer = new Resend(RESEND_KEY)
+    let message = formData.get('message')
     console.log({ formData })
-
-const resend = new Resend(process.env.RESEND_KEY);
-    resend.emails.send({
-        from: "daniel.camposarce@gmail.com",
-        to: "daniel.camposarce@gmail.com",
-        subject: " New Message From Website",
-        text:formData.get('message')
-    })
+    console.log({ msg: formData.get('message') })
+    Alert(`${message}`)
+    
 }
