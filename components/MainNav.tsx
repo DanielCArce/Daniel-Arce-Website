f9c1adf8bc2f15a84484962b26df3e1e31f5052d
DanielCArce <daniel.camposarce@gmail.com>
2024-08-05 00:43:26 -0600

    fix: hambuger menu

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import React, { useState, useEffect } from "react";
 import { usePathname } from 'next/navigation'
-import { FaBars } from "react-icons/fa";
+import { FaBars, FaAngleRight } from "react-icons/fa";
 function MainNav() {
   const route = usePathname()
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -11,30 +11,42 @@ function MainNav() {
     toggleMenu()
   },[route])
   return (
-    <nav className="text-gray-100">
-      <span className="absolute top-4 right-4 z-10 py-2 px-2 sm:flex md:hidden lg:hidden"><FaBars onClick={toggleMenu}/></span>
-        
-      <div className={isMenuOpen? "flex relative top-5 right-7":"hidden md:flex static"}>
-      <ul className="flex w-full h-screen pt-8 md:h-3/4 flex-col py-3 px-2 bg-black md:bg-transparent md:flex-row md:flex gap-3">
-          <li>
-            <Link href="/services">Services</Link>
-          </li>
-          <li>
-            <Link href="/about-me">About</Link>
-          </li>
-          <li>
-            <Link href="/blog">Blog</Link>
-          </li>
-          <li>
-            <Link
-              href="/contact"
-              className="border-white py-2 px-2 border-2 hover:border-orange-400"
-              >
-              Lets Talk
-            </Link>
-          </li>
+    <nav className="text-gray-200">
+      <span className="md:hidden lg:hidden sm:block">
+      <FaBars onClick={ toggleMenu} />
+      </span>
+      {
+        isMenuOpen ? <ul className="xsm:flex sm:flex xsm:flex-col sm:flex-col xsm:gap-2 sm:gap-2 md:hidden lg:hidden absolute py-1 px-1 bg-gray-900 w-3/4 h-screen top-0 right-0">
+      <span className="px-4 py-1"><FaAngleRight onClick={ toggleMenu}/></span>
+            <li>
+              <Link href={"/services"}>Services</Link>
+            </li>
+            <li>
+              <Link href={"/about-me"}>About</Link>
+            </li>
+            <li>
+              <Link href={"/contact"}>Contact</Link>
+            </li>
+            <li>
+              <Link href={"/blog"}>Blog</Link>
+            </li>
+
+        </ul> : null}
+      <ul className="xsm:hidden sm:hidden hidden md:flex lg:flex md:flex-row lg:flex-row md:gap-2 lg:gap-2">
+            <li>
+              <Link href={"/services"}>Services</Link>
+            </li>
+            <li>
+              <Link href={"/about-me"}>About</Link>
+            </li>
+            <li>
+              <Link href={"/contact"}>Contact</Link>
+            </li>
+            <li>
+              <Link href={"/blog"}>Blog</Link>
+            </li>
+
         </ul>
-      </div>
     </nav>
   );
 };
