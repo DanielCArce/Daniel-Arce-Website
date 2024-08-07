927a4f01c094a180c12e129b50120b4e42a5cd13
DanielCArce <daniel.camposarce@gmail.com>
2024-08-06 22:24:43 -0600

    added uppercase to header 1

--- components/CustomMDX.tsx
+++ components/CustomMDX.tsx
@@ -1,4 +1,4 @@
-export const H1Component = (props: any) => <h1 {...props} className="text-3xl font-bold">{props.children}</h1>
+export const H1Component = (props: any) => <h1 {...props} className="text-3xl font-bold uppercase">{props.children}</h1>
 export const H2Component = (props: any) => <h2 {...props} className="text-2xl font-bold">{props.children}</h2>
 export const H3Component = (props: any) => <h3 {...props} className="text-xl font-semibold">{props.children}</h3>
 export const PComponent = (props: any) => <p {...props} className="text-lg">{props.children}</p>
