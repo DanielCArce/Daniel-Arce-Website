8bea492eec046dce06fca947fbd9760a9a023d17
DanielCArce <daniel.camposarce@gmail.com>
2024-08-06 22:25:07 -0600

    minimus changes in page

--- app/(blog)/blog/page.tsx
+++ app/(blog)/blog/page.tsx
@@ -5,7 +5,7 @@ async function page() {
   const allBlogPostsSlugs = await getAllPosts()
 
   return (
-    <div className="px-5">
+    <div className="px-5 pt-7">
       <header>
         <h2 className="text-3xl font-semibold pl-2">Blog</h2>
         <h3 className="text-lg font-light text-italic">Some stories start with a post </h3>
