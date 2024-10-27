cca0365376ba8f3f7481010bb85070edd2150356
DanielCArce <daniel.camposarce@gmail.com>
2024-10-27 16:19:13 -0600

    feat: add content to sections

--- sections/ProjectsSection.tsx
+++ sections/ProjectsSection.tsx
@@ -1,8 +1,37 @@
+import ProjectCard, { ProjectCardProps } from '@/components/ProjectCard'
+import Link from 'next/link'
 import React from 'react'
-
+const allProjects: ProjectCardProps[] = [
+  {
+    id: 1,
+    name: 'Fina App',
+    type: 'Mobile App',
+    description: 'Streamline the orders from clients.',
+    links: 'fina-mobile-app',
+    imgs: {
+      webp: '/WebDev.webp',
+      jpg: 'WebDev.jpg',
+      alt:'Fina App Mobile App'
+    }
+  },
+  {
+    id: 2,
+    name: 'Distribuidora Catalina',
+    type: 'Website',
+    description: 'Factory website built with react & typescript.',
+    links: '/blog/distribuidora-catalina',
+    imgs: {
+      webp: '/AppDev.webp',
+      jpg: '/AppDev.jpg',
+      alt:'Distribuidora Catalina Website'
+    }
+  }
+]
 function ProjectsSection() {
   return (
-    <div>ProjectsSection</div>
+    <section id="projects" className="flex flex-row gap-2 h-screen container justify-between px-5 py-5">
+      {allProjects.map((project) => <ProjectCard key={ project.id} description={project.description} name={project.name} type={project.type} links={project.links} imgs={project.imgs}/>)}
+    </section>
   )
 }
 
--- sections/ServicesSection.tsx
+++ sections/ServicesSection.tsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 function ServicesSection() {
   return (
-    <div>ServicesSection</div>
+    <section className="flex flex-row h-screen gap-2 justify-between container px-5 py-5">
+      <div className="w-1/3">
+        <h2>Websites</h2>
+        <p>Design, Development & Hosting of websites</p>
+      </div>
+      <div className="w-1/3">
+        <h2>Mobile Apps</h2>
+        <p>Design, Development & publishing mobile apps in iOs & Android</p>
+      </div>
+      <div className="w-1/3">
+        <h2>Custom Software</h2>
+        <p>Adapting your issues to software with</p>
+      </div>
+    </section>
   )
 }
 
