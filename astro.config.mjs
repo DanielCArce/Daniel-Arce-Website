bab7b01b41215d8bc12f186250118dd5ac4dbaef
DanielCArce <daniel.camposarce@gmail.com>
2025-08-27 18:56:02 -0600

    add social links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import compression from 'vite-plugin-compression';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss(), ]
+    plugins: [tailwindcss(), compression({
+        algorithm: "gzip",
+        ext: ".gz",
+      }),
+      // Brotli
+      compression({
+        algorithm: "brotliCompress",
+        ext: ".br",
+      }) ]
   }
 });
