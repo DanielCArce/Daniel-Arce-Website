2c595866b8a4435a527cf8c24cd2569140ea940d
DanielCArce <daniel.camposarce@gmail.com>
2025-08-29 16:45:17 -0600

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import compression from 'vite-plugin-compression';
-import vercel from '@astrojs/vercel/server';
+import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
